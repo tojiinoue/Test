1380a5ab9a9959e19895876f0fe943e9d76f3c20
井上登司 <toji.inoue@inouenoborushinoMacBook-Air.local>
2024-09-06 02:14:04 +0900

    絞り込み機能をつける２

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Router> {/* HashRouterでAppをラップ */}
-            <App />
-        </Router>
-    </React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
