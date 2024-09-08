e4c810b6237844ff5bb147797143f67c1908475c
井上登司 <toji.inoue@inouenoborushinoMacBook-Air.local>
2024-09-09 05:11:31 +0900

    一括投稿

--- src/pages/list_quiz/components/quiz_simple.jsx
+++ src/pages/list_quiz/components/quiz_simple.jsx
@@ -2,6 +2,60 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./quiz_simple.css";
 
+
+function Time_diff(props) {
+    function convertSecondsToHours(secondsLimit, secondsStart) {
+        let isBeforeStartline = false;
+
+        const date1 = new Date(); // 現在時刻
+        const date2 = new Date(secondsLimit * 1000); // 終了時間
+        const date3 = new Date(secondsStart * 1000); // 開始時間
+
+        const epochTime1 = Math.floor(date1.getTime() / 1000); // 現在時刻をエポック時間に変換
+        const epochTime2 = Math.floor(date2.getTime() / 1000); // 終了時間をエポック時間に変換
+        const epochTime3 = Math.floor(date3.getTime() / 1000); // 開始時間をエポック時間に変換
+
+        let elapsedTime = 0;
+
+        if (epochTime1 < epochTime3) {
+            elapsedTime = Math.floor(Math.abs(epochTime3 - epochTime1));
+            elapsedTime = new Date(elapsedTime * 1000);
+            isBeforeStartline = true;
+        } else {
+            elapsedTime = Math.floor(Math.abs(epochTime2 - epochTime1));
+            elapsedTime = new Date(elapsedTime * 1000);
+        }
+
+        const labels = ["年", "ヶ月", "日", "時間", "分", "秒"];
+        let date = [];
+        date.push(elapsedTime.getUTCFullYear() - 1970);
+        date.push(elapsedTime.getUTCMonth());
+        date.push(elapsedTime.getUTCDate() - 1);
+        date.push(elapsedTime.getUTCHours());
+        date.push(elapsedTime.getUTCMinutes());
+        date.push(elapsedTime.getUTCSeconds());
+        let res = "";
+
+        for (let i = 0; i < date.length; i++) {
+            if (date[i] !== 0) {
+                res += date[i].toString() + labels[i];
+            }
+        }
+
+        if (isBeforeStartline) {
+            return "回答開始時間まで" + res;
+        } else {
+            return epochTime2 - epochTime1 > 0 ? "締め切りまで " + res : "締切終了";
+        }
+    }
+
+    return (
+        <div>
+            {convertSecondsToHours(parseInt(props.limit), parseInt(props.start))}
+        </div>
+    );
+}
+
 function Simple_quiz({ quiz }) {
     const [show, setShow] = useState(false);
     const [isReward, setIsReward] = useState(true);
