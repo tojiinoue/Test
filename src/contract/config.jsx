2c3dbc7c77c6c2c81cdab658da36ba0adaef6236
井上登司 <toji.inoue@inouenoborushinoMacBook-Air.local>
2024-09-27 11:15:50 +0900

    クイズコントラクトアドレスを変える

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -8,7 +8,8 @@ const rpc = "https://rpc-amoy.polygon.technology/"; // (required for Ethereum) J
 //const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
 //const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
-const quiz_address = "0x33cB33EBa9329639056E0ea79aC1e877eDBF4daa";
+//const quiz_address = "0x33cB33EBa9329639056E0ea79aC1e877eDBF4daa";
+const quiz_address = "0x59dBca45988b9c2E844A132002de28841013a004";
 const token_address = "0x4147aE2cEC45cFb2B9a9A3aE320C855DD767C2f8";
 
 export { chainId, rpc, quiz_address, token_address };
